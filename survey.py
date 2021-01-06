760fbddd277a6b92e118d972013f027be2180b9a
MayaDettwiler <73507307+MayaDettwiler@users.noreply.github.com>
2021-01-05 22:12:57 -0600

    Add files via upload

---
+++ survey.py
@@ -0,0 +1,31 @@
+'''
+Created on Jan 5, 2021
+program that takes survey information
+@author: ITAUser
+'''
+#Use input() to ask the user "What is your name?" and store that in
+#a variable called name. Then print the variable.
+name = "What is your name?"
+userResponse= input(name)
+print(userResponse)
+
+#Use input() to ask the user "What is your favorite color?" and store that 
+#in a variable called color. Then print the variable.
+color = "What is your color?"
+userResponse = input(color)
+print(userResponse)
+#Use input() to ask the user "What city did you grow up in?" and store 
+#that in a variable called city. Then print the variable.
+city = "What city did you grow up in?"
+userResponse = input(city)
+print(userResponse)
+#Use input() to ask the user "What is your best friends name?" and store 
+#that in a variable called friend. Then print the variable.
+friend = "What is your best friends name?"
+userResponse = input(friend)
+print(userResponse)
+#Use input() to ask the user "What color hair do you have?" and store that 
+#in a variable called hair. Then print the variable.
+hair = "What color hair do you have?"
+userResponse = input(hair)
+print(userResponse)
